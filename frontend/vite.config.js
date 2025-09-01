d67267e34857db027eadf143939bc733fc7364eb
enricorizzi-cmd <enrico.rizzi@dimensioneagricoltura.it>
2025-09-02 01:47:22 +0200

    frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,6 @@ const legacy = require('@vitejs/plugin-legacy').default;
 
 module.exports = {
   build: {
-    rollupOptions: {
-      input: 'main.js'
-    },
     outDir: 'dist'
   },
   plugins: [legacy()]
